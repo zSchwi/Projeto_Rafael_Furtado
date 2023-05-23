aa72fd3ba790e8cd0f894fe893e49d43caf0d8f8
zSchwi <122842926+zSchwi@users.noreply.github.com>
2023-05-23 20:54:34 -0300

    Adicionando arquivos
    Adicionando arquivos das classes A e B e um arquivo index.html

---
+++ Class_A.js
@@ -0,0 +1,35 @@
+class A {
+
+    constructor(A1, A2) {
+        this.A1 = A1
+        this.A2 = A2
+    }
+
+    getA1() {
+        return this.A1
+    }
+
+    setA1(parametroA1) {
+        this.A1 = parametroA1
+    }
+
+    getA2() {
+        return this.A2
+    }
+
+    setA2(parametroA2) {
+        this.A2 = parametroA2
+    }
+
+    MA1() {
+        console.log("MA1")
+    }
+
+    MA2() {
+        console.log("MA2")
+    }
+
+
+
+
+}
---
+++ Class_B.js
@@ -0,0 +1,33 @@
+class B {
+
+    constructor(B1, B2) {
+        this.B1 = B1
+        this.B2 = B2
+
+    }
+
+    getB1() {
+        return this.getB1
+    }
+
+    setB1(parametroB1) {
+        this.B1 = parametroB1
+    }
+
+    getB2() {
+        return this.B2
+    }
+
+    setB2(parametroB2) {
+        this.B2 = parametroB2
+    }
+
+
+    MB1() {
+        console.log("MB1")
+    }
+
+    MB2() {
+        console.log("MB2")
+    }
+}
