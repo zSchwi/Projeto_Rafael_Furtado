7c10644e9e46bf73e530ef6683bcae27dbcd49b3
Rafael Furtado Morais <rafa.fmorais2001@gmail.com>
2023-05-23 21:18:19 -0300

    Criação da Classe C

---
+++ Class_C.js
@@ -0,0 +1,15 @@
+class C {
+    constructor(C1, C2) {
+        this.C1 = C1
+        this.C2 = C2
+    }
+
+    MC1() {
+        console.log("MC1")
+    }
+
+    MC2() {
+        console.log("MC2")
+    }
+
+}
